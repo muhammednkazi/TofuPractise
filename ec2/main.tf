2dc10d5bcd61c8c999c7a6279c0d9955df22c191
Muhammed Kazi <56946257+muhammednkazi@users.noreply.github.com>
2025-12-02 10:05:52 +0000

    updated

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,15 @@
 resource "aws_instance" "example" {
+  #count         = 2
   ami           = "ami-02d26659fd82cf299"
-  instance_type = "t2.medium"
+  instance_type =  "t2.micro" #var.env == "dev" ? "t2.micro" : "t2.medium"
   key_name      = "TofuPractise"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "demo" #"Instance-${count.index+1}"
   }
+}
+
+
+variable "env" {
+  
 }
